67c0e45e720c38eaf9f959e5734800c6c4a73329
Cesar Santos <C3S4R780@gmail.com>
2023-04-06 21:00:17 -0300

    [Change] Criado função asincrona que envia dados do sensor para API ThingSpeak

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
-# import esptool
 import machine
-from time import sleep
+import dht
+import urequests
+import uasyncio
 
 rele = machine.Pin(2, machine.Pin.OUT)
-i = 0
-while (i < 5):
-    rele.value(1)
-    sleep(1)
-    rele.value(0)
-    sleep(1)
-    i += 1
+sensor = dht.DHT11(machine.Pin(4))
+event_loop = uasyncio.get_event_loop()
+
+rele.value(1)
+    
+async def main():
+    while True:
+
+        try: sensor.measure()
+        except: continue
+
+        req = urequests.get(
+            "https://api.thingspeak.com/update?api_key=30YK09A9EY3SHNGY&field1={}&field2={}"
+            .format(
+                str(sensor.temperature()),
+                str(sensor.humidity())
+            )
+        )
+        req.close()
+        await uasyncio.sleep(60)
+
+event_loop.create_task(main())
+event_loop.run_forever()
