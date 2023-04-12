e670f8f52525f188018211e9b155997714b3e643
Cesar Santos <C3S4R780@gmail.com>
2023-04-12 18:42:28 -0300

    [Changed] Using relay as visual feedback for the request

--- main.py
+++ main.py
@@ -6,14 +6,14 @@ import uasyncio
 rele = machine.Pin(2, machine.Pin.OUT)
 sensor = dht.DHT11(machine.Pin(4))
 event_loop = uasyncio.get_event_loop()
-
-rele.value(1)
     
 async def main():
     while True:
 
         try: sensor.measure()
         except: continue
+        
+        rele.value(1)
 
         req = urequests.get(
             "https://api.thingspeak.com/update?api_key=30YK09A9EY3SHNGY&field1={}&field2={}"
@@ -22,7 +22,12 @@ async def main():
                 str(sensor.humidity())
             )
         )
+
+        if req.status_code == 200:
+            rele.value(0)
+
         req.close()
+
         await uasyncio.sleep(60)
 
 event_loop.create_task(main())
